2bd1dc435bb8c068ba3a0dc58aa37dc8af4f19b2
Bruno Soares Esteves <bruno.soares@basisti.com.br>
2025-07-21 16:20:29 -0300

    add platform-server

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { ServerModule } from '@angular/platform-server';
 
 @NgModule({
   imports: [
     AppModule,
+    ServerModule
   ],
   bootstrap: [AppComponent],
 })
